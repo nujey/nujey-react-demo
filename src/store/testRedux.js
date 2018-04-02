0e11d7d4ac73e9edf4dba3c62270df53e2a56d0d
张锋 <zhangfeng@xkeshi.com>
2018-04-02 22:10:29 +0800

    redux的例子

--- src/store/testRedux.js
+++ src/store/testRedux.js
@@ -10,3 +10,34 @@ import { createStore } from 'redux'
  * 下面例子使用switch语句和字符串来判断 但是你可以写帮助类
  * 根据不同的约定（比如一个方法的映射）来判断 只要适合你的项目就可以
  */
+function counter(state = 0, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+}
+// 创建Redux store来存放应用的状态
+// API 是{ subscribe, dispatch, getState }
+let store = createStore(counter);
+// 可以手动订阅更新 也可以事件绑定到视图层
+store.subscribe(() => {
+  console.log(store.getState())
+})
+
+// 改变内部吧state 唯一的方法就是dispatch action
+// action可以被序列化 用日记记录和存储下来 后来还可以以回放的方式执行
+
+// 1
+store.dispatch({ type: 'INCREMENT' })
+// 2
+store.dispatch({ type: 'INCREMENT' })
+// 1
+store.dispatch({ type: 'DECREMENT' })
+
+// 你应该把要做的修改变成一个普通对象，这个对象被叫做 action，而不是直接修改 state
+// 然后编写专门的函数来决定每个 action 如何改变应用的 state，这个函数被叫做 reducer
+// 这里需要注意的是 以前的flux是支持多个store的 但是redux只允许有一个数据池
