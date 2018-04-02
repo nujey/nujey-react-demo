2a1ab42e90046177efb1cfaa5aa1f887cf516094
张锋 <zhangfeng@xkeshi.com>
2018-04-02 23:12:47 +0800

    reducer

--- src/store/testRedux2.js
+++ src/store/testRedux2.js
@@ -28,3 +28,38 @@ store.dispatch({
 // 我们用纯函数来执行修改
 // 为了描述axtion如何改变state tree 我们需要编写reducers
 // Reducer 只是一些纯函数，它接收先前的 state 和 action，并返回新的 state
+function visibilityFilter(state = 'SHOW_ALL', action) {
+  switch (action.type) {
+    case 'SET_VISIBILITY_FILTER':
+      return action.filter
+    default:
+      return state
+  }
+}
+function todos(state = [], action) {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        {
+          text: action.text,
+          completed: false
+        }
+      ]
+    case 'COMPLETE_TODO':
+      return state.map((todo, index) => {
+        if (index === action.index) {
+          return Object.assign({}, todo, {
+            completed: true
+          })
+        }
+        return todo
+      })
+    default:
+      return state
+  }
+}
+
+import { combineReducers, createStore } from 'redux'
+let reducer = combineReducers({ visibilityFilter, todos })
+let store = createStore(reducer)
