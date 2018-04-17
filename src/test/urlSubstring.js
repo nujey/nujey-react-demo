39a0f1f7b4228fcb53e3a4121b61bb2d9819b06a
张锋 <zhangfeng@xkeshi.com>
2018-04-17 21:58:02 +0800

    url

---
+++ src/test/urlSubstring.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+export class SubstringUrl extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleUrl = this.handleUrl.bind(this)
+    this.state = {
+      url: 'www.zhangfeng.com?id=1&name=zhangfeng&age=18'
+    }
+  }
+  handleUrl() {
+    // 获取一个一个url后面的参数
+    const str = this.state.url.substring(this.state.url.indexOf('?') + 1)
+    const arr = str.split('&')
+    let newArr = []
+    let params = {}
+    arr.forEach((e, index) => {
+      newArr = e.split('=')
+      params[newArr[0]] = newArr[1]
+    })
+    this.setState({
+      str: str,
+      params: [params]
+    })
+  }
+  render() {
+    const listItem = this.state.params
+    let listItems = []
+    if (listItem) {
+      listItems = listItem.map((item) =>
+        <span>{item.name}2222</span>
+      )
+    }
+    return (
+      <div>
+        {listItem ? listItem[0].name : 0}
+        <button onClick={this.handleUrl}>惦记我哦</button>
+      </div>
+    )
+  }
+}
