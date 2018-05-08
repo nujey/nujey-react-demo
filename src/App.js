96556785d0d40a0e808eb63b7a1d68183a6317cf
张锋 <zhangfeng@xkeshi.com>
2018-05-08 23:25:03 +0800

    composition

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ import GrandParent from './componets/parentRef'
 import Unique from './componets/unique'
 import ObjArr from './test/objarr'
 import TimeMoment from './test/timeMoment'
+import Composition from './componets/composition'
 // 在使用我们的组件的时候 必须是首字母大写 这样是为了让react知道 是HTML的元素
 // 还是我们自定义的组件类 如果不大写 那么react是没办法认识的 所以也就得不到我们想要的结果
 
@@ -96,6 +97,8 @@ class App extends Component {
         <h2>=====这里是一个组件分界线=====</h2>
         <h2>=====这里是一个组件数组的排序去重=====</h2>
         <ObjArr />
+        <h2>=====Composition=====</h2>
+        <Composition />
       </div>
     );
   }
